4f93c1d1e41f0b9cb9f1a708f462e033ae4a44ca
bunsung92 <bunsung92@naver.com>
2022-06-19 19:00:37 +0900

    feat: add Pageable and Sort test

--- boot-web/src/test/java/com/calmdev/ex1/repository/MemoRepositoryTests.java
+++ boot-web/src/test/java/com/calmdev/ex1/repository/MemoRepositoryTests.java
@@ -4,6 +4,10 @@ import com.calmdev.ex1.entity.Memo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -52,15 +56,49 @@ public class MemoRepositoryTests {
     public void testUpdate() {
 
         Memo memo = Memo.builder()
-                .memoText("Update Tesxt")
+                .memoText("Update Text")
                 .build();
         System.out.println(memoRepository.save(memo));
     }
 
     @Test
     public void testDelete() {
-        Long mno = 100L;
 
+        Long mno = 100L;
         memoRepository.deleteById(mno);
+    }
+
+    @Test
+    public void testPageDefault() {
+        // 1페이지 10개
+        Pageable pageable = PageRequest.of(0,10);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        System.out.println(result);
+
+        System.out.println("-------------------------------------------------");
+        System.out.println("Total Pages : " + result.getTotalPages());
+        System.out.println("Total Count : " + result.getTotalElements());
+        System.out.println("Page Number: " + result.getNumber()); // 현재 페이지 번호
+        System.out.println("Page Size : " + result.getSize()); // 페이지당 데이터 개수
+        System.out.println("has next page? : " + result.hasNext());
+        System.out.println("first page? : " + result.isFirst());
+
+        System.out.println("-------------------------------------------------");
+        for (Memo memo : result.getContent()) {
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testSort() {
+        //Sort 는 한 개 혹은 여러 개의 필드 값을 이용해서 순차적으로 (asc) 이나 역순으로 정렬(desc) 을 지정할 수 있음.
+        Sort sort1 = Sort.by("mno").descending();
+        Sort sort2 = Sort.by("memoText").ascending();
+        Sort sortAll = sort1.and(sort2);        //and 를 이용한 연결.
+
+        Pageable pageable = PageRequest.of(0,10,sortAll);
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        result.get().forEach(System.out::println);
     }
 }
