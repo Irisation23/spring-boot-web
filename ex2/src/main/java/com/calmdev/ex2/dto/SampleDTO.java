877ec3260909a4e0d80a9b89612f27477d1b5faf
bunsung92 <bunsung92@naver.com>
2022-06-21 01:29:11 +0900

    feat: add thymeleaf if unless

---
+++ ex2/src/main/java/com/calmdev/ex2/dto/SampleDTO.java
@@ -0,0 +1,16 @@
+package com.calmdev.ex2.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+}
